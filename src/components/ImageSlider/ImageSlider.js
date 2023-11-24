98b526e56bab7ae7aba31dcee38aa9de5267d651
NorthWillov <northwillov@gmail.com>
2023-11-24 14:49:17 +0100

    Add content to carousel

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -3,6 +3,10 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import logomain from "../../assets/logomain.svg";
+import heart from "../../assets/heart.svg";
+import messenger from "../../assets/messenger.svg";
+import youtube from "../../assets/youtube.svg";
+import tbngo from "../../assets/tbngo.svg";
 import styles from "./ImageSlider.module.css";
 
 const images = ["image1.jpeg", "image2.jpeg", "image3.jpeg", "image4.jpeg"];
@@ -53,6 +57,63 @@ const ImageSlider = () => {
             onClick={() => handleDotClick(index)}
           ></div>
         ))}
+      </div>
+      <div className={styles.icons}>
+        <div className={styles.iconWrapper}>
+          <Image
+            className={styles.logo}
+            style={{ marginBottom: "-20px" }}
+            src={heart}
+            alt="serce"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className={styles.iconWrapper}>
+          <Image
+            className={styles.logo}
+            style={{ marginBottom: "-20px" }}
+            src={messenger}
+            alt="messenger"
+            width="100"
+            height="100"
+          />
+        </div>
+        <div className={styles.iconWrapper}>
+          <Image
+            className={styles.logo}
+            style={{ marginBottom: "-20px" }}
+            src={youtube}
+            alt="youtube"
+            width="80"
+            height="100"
+          />
+        </div>
+        <div className={styles.iconWrapper}>
+          <Image
+            className={styles.logo}
+            src={tbngo}
+            alt="tbngo"
+            width="80"
+            height="100"
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.contentTitle}>Lorem ipsum! Lorem</h1>
+        <div className={styles.contentTextWrapper}>
+          <p className={styles.contentText}>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+            velit esse molestie consequat, vel illum dolore eu feugiat nulla
+            facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi.
+          </p>
+        </div>
       </div>
       <div className={styles.logoWrapper}>
         <Image
