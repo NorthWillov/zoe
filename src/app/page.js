a096489ee6f97c5fd336e6328b163c42e4b364bf
NorthWillov <northwillov@gmail.com>
2023-11-26 11:40:06 +0100

    Add one section about

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import ImageSlider from "@/components/ImageSlider/ImageSlider";
 import Navbar from "@/components/Navbar/navbar";
+import About from "@/components/About/about";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
       <ImageSlider />
+      <About />
     </main>
   );
 }
